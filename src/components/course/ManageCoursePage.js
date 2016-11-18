5a5528870a3a2286c746cea26553f8421ba2f0ce
Sid Ngeth <sngeth@gmail.com>
2016-11-18 16:29:04 -0500

    Add spec for ManageCoursePage author selector

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -5,6 +5,7 @@ import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
 import CourseForm from './CourseForm';
 import toastr from 'toastr';
+import {authorsFormattedForDropdown} from '../../selectors/selectors';
 
 export class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -107,16 +108,9 @@ function mapStateToProps(state, ownProps) {
     course = getCourseById(state.courses, courseId);
   }
 
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + ' ' + author.lastName
-    };
-  });
-
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
