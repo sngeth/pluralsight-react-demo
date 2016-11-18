6a7786b09d80313e51503791b7ea3b5feded68a3
Sid Ngeth <sngeth@gmail.com>
2016-11-18 12:45:16 -0500

    Handle create and updates for course

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -14,3 +14,22 @@ export function loadCourses() {
     });
   };
 }
+
+export function createCourseSuccess(course) {
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function updateCourseSuccess(course) {
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 import {bindActionCreators} from 'redux';
 import CourseList from './CourseList';
+import { browserHistory } from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -11,10 +12,16 @@ class CoursesPage extends React.Component {
     this.state = {
       course: { title: "" }
     };
+
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
+  }
+
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
   }
 
   render() {
@@ -24,6 +31,10 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         <CourseList courses={courses}/>
+        <input type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}/>
       </div>
     );
   }
