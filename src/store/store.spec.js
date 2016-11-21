96761dda21238c14d07f5f01f430cdc43108c24e
Sid Ngeth <sngeth@gmail.com>
2016-11-21 15:39:09 -0500

    Add integration test for creating course

---
+++ src/store/store.spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+import * as courseActions from '../actions/courseActions';
+import initialState from '../reducers/initialState';
+
+describe('Store', () => {
+  it('Should handle creating courses', () => {
+    const store = createStore(rootReducer, initialState);
+    const course = {
+      title: "Clean Code"
+    }
+
+    const action = courseActions.createCourseSuccess(course);
+    store.dispatch(action);
+
+    const actual = store.getState().courses[0];
+    expect(actual).toEqual(course);
+  })
+})
